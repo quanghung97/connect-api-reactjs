82b6105fc24063badce5dfa190d7ba00936ec98b
quanghung97 <hungcan1997@gmail.com>
2018-08-27 00:32:53 +0700

    store product with API on form

--- src/pages/ProductActionPage/ProductActionPage.js
+++ src/pages/ProductActionPage/ProductActionPage.js
@@ -1,41 +1,79 @@
 import React, {Component} from 'react';
+import callApi from './../../utils/apiCaller';
 
 class ProductActionPage extends Component {
 
-    // constructor(props){
-    //     super(props);
-    //     this.state({
-    //         aaa : ''
-    //     });
-    // }
-    //
-    // onChange = (event) => {
-    //     var target = event.target;
-    //     var name = target.name;
-    // }
+    constructor(props){
+        super(props);
+        this.state = {
+            id : '',
+            txtName : '',
+            txtPrice : '',
+            chkbStatus : ''
+        };
+    }
+
+    onChange = (event) => {
+        var target = event.target;
+        var name = target.name;
+        var value = target.type === 'checkbox' ? target.checked : target.value;
+        this.setState({
+            [name] : value
+        });
+    }
+
+    onSave = (event) => {
+        event.preventDefault();
+        var { txtName, txtPrice, chkbStatus } = this.state;
+        callApi('products', 'POST', {
+            name : txtName,
+            price : txtPrice,
+            status : chkbStatus
+        }).then(res => {
+            console.log(res);
+        });
+    }
 
     render() {
 
+        var {txtName, txtPrice, chkbStatus} = this.state;
+
         return (
             <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                <form>
+                <form onSubmit={this.onSave}>
 
 
                          <div className="form-group">
                             <label className="ml-1000">Tên sản phẩm: </label>
-                            <input type="text" className="form-control" placeholder="Input field" />
+                            <input
+                                type="text"
+                                name="txtName"
+                                value = {txtName}
+                                onChange = {this.onChange}
+                                className="form-control"
+                                placeholder="Input field" />
                          </div>
 
                          <div className="form-group">
                             <label className="ml-1000">Giá: </label>
-                            <input type="text" className="form-control" placeholder="Input field" />
+                            <input
+                                type="text"
+                                name="txtPrice"
+                                value = {txtPrice}
+                                onChange = {this.onChange}
+                                className="form-control"
+                                placeholder="Input field" />
                          </div>
 
                          <div className="form-group">
                             <label className="ml-1000">Trạng thái: </label>
                             <div className="checkbox form-group">
                                 <label className="ml-300">
-                                    <input type="checkbox"/>
+                                    <input
+                                        type="checkbox"
+                                        name="chkbStatus"
+                                        value = {chkbStatus}
+                                        onChange = {this.onChange}/>
                                     Còn Hàng
                                 </label>
                             </div>
--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -3,6 +3,7 @@ import ProductList from './../../components/ProductList/ProductList';
 import ProductItem from './../../components/ProductItem/ProductItem';
 import { connect } from 'react-redux';
 import callApi from './../../utils/apiCaller';
+import { Link } from 'react-router-dom';
 
 class ProductListPage extends Component {
 
@@ -28,7 +29,7 @@ class ProductListPage extends Component {
 
         return (
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                     <button type="button" className="btn btn-info mb-10 ">Thêm Sản Phẩm</button>
+                     <Link to="/product/add" className="btn btn-info mb-10 ">Thêm Sản Phẩm</Link>
                      <br />
                      <ProductList>
                          {this.showProducts(products)}
