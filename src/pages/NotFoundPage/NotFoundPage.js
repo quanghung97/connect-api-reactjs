b9cacb3c481c453ee3e14fe8266e84e9677ccf9d
quanghung97 <hungcan1997@gmail.com>
2018-08-26 22:03:26 +0700

    import and setting view react-router

---
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class NotFoundPage extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div class="alert alert-warning">
+                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                  <strong>Không tìm thấy trang!</strong>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default NotFoundPage;
