8588d84f425ad0b3cba3bbc16b3a47bb86b3b6b2
quanghung97 <hungcan1997@gmail.com>
2018-08-27 21:41:28 +0700

    edit product call Api

--- src/pages/ProductActionPage/ProductActionPage.js
+++ src/pages/ProductActionPage/ProductActionPage.js
@@ -41,16 +41,26 @@ class ProductActionPage extends Component {
 
     onSave = (event) => {
         event.preventDefault();
-        var { txtName, txtPrice, chkbStatus } = this.state;
+        var { id, txtName, txtPrice, chkbStatus } = this.state;
         var { history } = this.props;
-        callApi('products', 'POST', {
-            name : txtName,
-            price : txtPrice,
-            status : chkbStatus
-        }).then(res => {
-            history.goBack();
-            // history.push("/");
-        });
+        if (id) { //update
+            callApi(`products/${id}`, 'PUT', {
+                name : txtName,
+                price : txtPrice,
+                status : chkbStatus
+            }).then(res => {
+                history.goBack();
+            });
+        } else {
+            callApi('products', 'POST', {
+                name : txtName,
+                price : txtPrice,
+                status : chkbStatus
+            }).then(res => {
+                history.goBack();
+                // history.push("/");
+            });
+        }
     }
 
     render() {
