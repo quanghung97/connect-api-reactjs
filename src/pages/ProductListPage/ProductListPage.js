d4abd3bebd7309f1d2702d31f6f39363095691c5
quanghung97 <hungcan1997@gmail.com>
2018-08-27 21:12:27 +0700

    delete one Product from list Products

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -23,6 +23,32 @@ class ProductListPage extends Component {
         });
     }
 
+    onDelete = (id) => {
+        //console.log(id);
+        var {products} = this.state;
+        callApi(`products/${id}`, 'DELETE', null).then(res => {
+            if(res.status === 200){//ok
+                var index = this.findIndex(products, id);
+                if (index !== -1) {
+                    products.splice(index, 1);
+                    this.setState({
+                        products : products
+                    });
+                }
+            }
+        });
+    }
+
+    findIndex = (products, id) => {
+        var result = -1;
+        products.forEach((product, index) => {
+            if (product.id === id){
+                result = index;
+            }
+        });
+        return result;
+    }
+
     render() {
         // var { products } = this.props;
         var {products} = this.state;
@@ -47,6 +73,7 @@ class ProductListPage extends Component {
                         key={index}
                         product={product}
                         index={index}
+                        onDelete = {this.onDelete}
                     />
                 );
             });
